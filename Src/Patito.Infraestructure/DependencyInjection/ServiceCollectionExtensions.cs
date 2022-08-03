d238a2b93c17e4ab96c08f51cf1d6f4f78fc93c3
Samuel Loza <starsaminf@gmail.com>
2022-08-03 04:13:09 -0400

    Add DependencyInjection

---
+++ Src/Patito.Infraestructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Patito.Infraestructure.DependencyInjection
+{
+    public class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }        
+    }
+}
